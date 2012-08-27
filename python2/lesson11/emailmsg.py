b6d2e17e92b4a1286403aa409d1de42eaef86fc2
VincentYing <vhying82@gmail.com>
2012-08-26 20:36:53 -0700

    Setting email attachments with the correct MIME type and also added a test case.

--- python2/lesson11/emailmsg.py
+++ python2/lesson11/emailmsg.py
@@ -1,8 +1,12 @@
 from email.mime.multipart import MIMEMultipart
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
 from email.mime.text import MIMEText
-from email.mime.base import *
 import mimetypes
 import os
+
+PATHSTEM = "v:\\workspace\\Python2_Homework11\\src\\"
 
 def emailmsg(addy, body, attachments):
     msg = MIMEMultipart()
@@ -10,12 +14,34 @@ def emailmsg(addy, body, attachments):
     msg['To'] = addy
     text_msg = MIMEText(body, 'plain')
     msg.attach(text_msg)
-    
-    for fn in attachments:
-        format, enc = mimetypes.guess_type(fn)
-        mb = MIMEBase(*format.split('/'))
-        mb.set_payload(open(fn, 'rb').read())
-        mb.add_header('Content-Disposition', 'attachment', filename=os.path.basename(fn))
-        msg.attach(mb)
+
+    if attachments:
+        for fn in attachments:
+            format, enc = mimetypes.guess_type(fn)
+            type, subtype = format.split('/')
+            
+            if type == "text":
+                fp = open(fn)
+                mime = MIMEText(fp.read(), _subtype=subtype)
+            elif type == "image":
+                fp = open(fn, 'rb')
+                mime = MIMEImage(fp.read(), _subtype=subtype)
+            elif type == 'audio':
+                fp = open(fn, 'rb')
+                mime = MIMEAudio(fp.read(), _subtype=subtype)
+            else:
+                fp = open(fn, 'rb')
+                mime = MIMEBase(type, subtype)
+                mime.set_payload(fp.read())
+            
+            mime.add_header('Content-Disposition', 'attachment', filename=os.path.basename(fn))
+            msg.attach(mime)
+            fp.close()
     
     return msg
+
+if __name__ == "__main__":
+    attach1 = PATHSTEM + "python_logo.png"
+    attach2 = PATHSTEM + "lorem.txt"
+    attachments = [attach1, attach2]
+    print(emailmsg("ying82@gmail.com", "Email body.", attachments))
